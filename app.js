66ab60c1c879feea7d635a92999e57a3945f4260
CantCommitToAnything <kiqbal@jctnj.com>
2026-05-08 08:50:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ const defaultSystems = {
         label: "360 Camera",
         symbol: "360",
         prefix: "CAM360",
-        iconImage: "icons/360cam.png",
+        iconImage: "icons/360Cam.png",
         iconSize: 17
       },
 
